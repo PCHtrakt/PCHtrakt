78216f758fdba59902439243c06a80a2d84a55e6
cptjhmiller <jamied@blueyonder.co.uk>
2012-08-18 22:02:54 +0200

    ISO and VIDEO_TS scrobbling #2

--- pchtrakt/scrobble.py
+++ pchtrakt/scrobble.py
@@ -39,12 +39,12 @@ def showStarted(myMedia):
         pchtrakt.logger.info(msg)
         if response != None:
             msg = 'Video playing: %s - %s' %(response['status'],response['message'])
-            
+
         else:
             msg = 'No response from Trakt.tv'
         Debug(msg)
         pchtrakt.logger.info(msg)
-    
+
 def movieStarted(myMedia):
     response = utilities.watchingMovieOnTrakt(myMedia.parsedInfo.id,
                                                myMedia.parsedInfo.name,
@@ -54,46 +54,46 @@ def movieStarted(myMedia):
     if response != None:
         msg = 'Video playing: %s - %s' %(response['status'],response['message'])
     else:
-        msg = 'No response from Trakt.tv'    
+        msg = 'No response from Trakt.tv'
     Debug(msg)
     pchtrakt.logger.info(msg)
-    
-    
+
+
 def showStopped():
     response = utilities.cancelWatchingEpisodeOnTrakt()
     if response != None:
         msg = 'Video stopped: %s - %s' %(response['status'],response['message'])
     else:
-        msg = 'No response from Trakt.tv' 
+        msg = 'No response from Trakt.tv'
     Debug(msg)
     pchtrakt.logger.info(msg)
-    
-    
+
+
 def movieStopped():
     response = utilities.cancelWatchingMovieOnTrakt()
     if response != None:
         msg = 'Video stopped: %s - %s' %(response['status'],response['message'])
     else:
-        msg = 'No response from Trakt.tv' 
+        msg = 'No response from Trakt.tv'
     Debug(msg)
     pchtrakt.logger.info(msg)
-    
-    
+
+
 def videoStopped():
     if pchtrakt.isTvShow and TraktScrobbleTvShow:
         showStopped()
     elif pchtrakt.isMovie and TraktScrobbleMovie:
         movieStopped()
-    
-    
+
+
 def showStillRunning(myMedia):
     showStarted(myMedia)
-    
-    
+
+
 def movieStillRunning(myMedia):
     movieStarted(myMedia)
-    
-    
+
+
 def showIsEnding(myMedia):
     try:
         if BetaSeriesScrobbleTvShow:
@@ -101,7 +101,7 @@ def showIsEnding(myMedia):
             serieXml = bs.getSerieUrl(myMedia.parsedInfo.name)
             token = bs.getToken()
             isWatched = bs.isEpisodeWatched(serieXml,token,myMedia.parsedInfo.season_number
-                                        ,myMedia.parsedInfo.episode_numbers[myMedia.idxEpisode])      
+                                        ,myMedia.parsedInfo.episode_numbers[myMedia.idxEpisode])
             Debug('(BetaSeries) Is episode watched: {0}'.format(isWatched))
             msg = '(BetaSeries) Video is '
             if not isWatched:
@@ -121,7 +121,7 @@ def showIsEnding(myMedia):
                                            )
                 Debug(msg)
                 pchtrakt.logger.info(msg)
-                
+
         else:
             myMedia.ScrobResult |= EnumScrobbleResult.BETASERIESOK
     except BetaSerieAuthenticationException as e:
@@ -144,14 +144,14 @@ def showIsEnding(myMedia):
             Debug(msg)
             pchtrakt.logger.info(msg)
             result = 1
-        
+
         if result == 1:
             myMedia.ScrobResult |= EnumScrobbleResult.TRAKTOK
-    else:  
+    else:
         myMedia.ScrobResult |= EnumScrobbleResult.TRAKTOK
     return myMedia.ScrobResult == EnumScrobbleResult.TRAKTOK | EnumScrobbleResult.BETASERIESOK
-    
-    
+
+
 def movieIsEnding(myMedia):
     response = utilities.scrobbleMovieOnTrakt(myMedia.parsedInfo.id,
                                                myMedia.parsedInfo.name,
@@ -165,7 +165,7 @@ def movieIsEnding(myMedia):
         return 1
     return 0
 
-    
+
 def videoStatusHandleMovie(myMedia):
     if pchtrakt.lastPath != myMedia.oStatus.fullPath:
         pchtrakt.watched = 0
@@ -188,7 +188,7 @@ def videoStatusHandleMovie(myMedia):
         pchtrakt.watched = 0
         pchtrakt.currentTime = myMedia.oStatus.currentTime
         movieStarted(myMedia)
-            
+
 def videoStatusHandleTVSeries(myMedia):
     if len(myMedia.parsedInfo.episode_numbers)>1:
             doubleEpisode = 1
@@ -210,17 +210,17 @@ def videoStatusHandleTVSeries(myMedia):
                 pchtrakt.currentTime = myMedia.oStatus.currentTime
             else:
                 showStarted(myMedia)
-    if not pchtrakt.watched: 
+    if not pchtrakt.watched:
         if myMedia.oStatus.percent > 90:
             pchtrakt.watched = showIsEnding(myMedia)
         elif myMedia.oStatus.currentTime > pchtrakt.currentTime + int(TraktRefreshTime)*60:
             pchtrakt.currentTime = myMedia.oStatus.currentTime
-            showStillRunning(myMedia)        
+            showStillRunning(myMedia)
         elif doubleEpisode and myMedia.oStatus.percent > (myMedia.idxEpisode+1) * 90.0/len(myMedia.parsedInfo.episode_numbers) and myMedia.idxEpisode+1 < len(myMedia.parsedInfo.episode_numbers):
             showIsEnding(myMedia)
             myMedia.idxEpisode += 1
             showStarted(myMedia)
-                
+
     elif myMedia.oStatus.percent < 10 and myMedia.oStatus.status != EnumStatus.STOP:
         Debug('It seems you came back at the begining of the video... so I say to trakt it\'s playing')
         pchtrakt.watched = 0
@@ -246,13 +246,13 @@ def videoStatusHandle(myMedia):
 
 def isIgnored(myMedia):
     ignored = False
-    
+
     msg = u'File: {0}'.format(myMedia.oStatus.fileName)
     Debug(msg)
     pchtrakt.logger.info(msg)
-    
+
     ignored = isKeywordIgnored(myMedia.oStatus.fileName)
-    
+
     if not ignored and ignored_repertory[0] != '':
         for el in myMedia.oStatus.fullPath.split('/'):
             if el <> '' and el in ignored_repertory:
@@ -275,7 +275,7 @@ def isIgnored(myMedia):
             file = unicode(myMedia.oStatus.fileName.rsplit('.',1)[0] + '.xml', errors='replace')
             oXml = ElementTree.parse(YamjPath + file)
             genres = oXml.findall('.//genre')
-            
+
             ignored = isGenreIgnored(genres)
     return ignored
 
@@ -288,7 +288,7 @@ def isKeywordIgnored(title):
                 pchtrakt.logger.info(msg)
                 return True
     return False
-    
+
 def isGenreIgnored(genres):
     txt = 'The ignored genres are :{0}'.format(YamjIgnoredCategory)
     Debug(txt)
@@ -305,18 +305,23 @@ def isGenreIgnored(genres):
             pchtrakt.logger.info(txt)
             return True
     return False
-    
+
 def watchedFileCreation(myMedia):
     if myMedia.oStatus.percent > 90:
         path = myMedia.oStatus.fileName
         if YamJWatchedVithVideo:
             path = myMedia.oStatus.fullPath
+			#Remember that .DVD extension
+            if (path.split(".")[-1] == "DVD"):
+                path = path[:-4]
             if not OnPCH:
                 path = path.replace('/opt/sybhttpd/localhost.drives/','')
                 path = path.split('/', 2)[2]
                 path = '{0}{1}'.format(YamjWatchedPath, path)
         else:
             path = '{0}{1}'.format(YamjWatchedPath, path)
+            if (path.split(".")[-1] == "DVD"):
+                path = path[:-4]
         path = '{0}.watched'.format(path)
         if not isfile(path):
             f = open(path, 'w')
